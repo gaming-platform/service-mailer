097874fe8476e35eafbf9d4965976a6f821396f0
Markus Reinhold <markusreinhold@icloud.com>
2019-04-26 22:41:20 +0200

    Exceptions extending from super type

--- src/Domain/Template/Exception/RenderFailedException.php
+++ src/Domain/Template/Exception/RenderFailedException.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace GamingPlatform\Mailer\Domain\Template\Exception;
 
-final class RenderFailedException extends \Exception
+final class RenderFailedException extends TemplateException
 {
 
 }
--- src/Domain/Template/Exception/TemplateNotFoundException.php
+++ src/Domain/Template/Exception/TemplateNotFoundException.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace GamingPlatform\Mailer\Domain\Template\Exception;
 
-final class TemplateNotFoundException extends \Exception
+final class TemplateNotFoundException extends TemplateException
 {
 
 }
